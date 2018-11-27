166ae5e267f185640ae01d64becaf13f420fd6a3
Travis Cooper <dozertdc@gmail.com>
2018-11-26 21:40:09 -0500

    Update isUnique.js

--- CrackingTheCodingInterview/1.1/isUnique.js
+++ CrackingTheCodingInterview/1.1/isUnique.js
@@ -13,6 +13,19 @@ function isUnique(input){
 
 }
 
+function isUniqueNoDataStructures(input){
+
+    for(var i=0; i<input.length; i++){
+        for(var j=i+1;j<input.length; j++){
+            if(input[i] === input[j]){
+                return false;
+            }
+        }
+    }
+    return true;
+
+}
+
 console.log('pancakes');
 console.log(isUnique('pancakes'));
 console.log('able');
