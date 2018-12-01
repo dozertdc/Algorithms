8a8dedf964d2315e1dbec29a33a7b01bd0b44e81
Travis Cooper <dozertdc@gmail.com>
2018-11-30 20:58:03 -0500

    Update maxSubarraySum.js

--- Basic/sliding_window/maxSubarraySum.js
+++ Basic/sliding_window/maxSubarraySum.js
@@ -1,15 +1,24 @@
+// Time Complexity 0(n)
+// Sliding Window Technique
 function maxSubarraySum(arr, num){
   let maxSum = 0;
   let tempSum = 0;
+  // Loop Through first n terms to get initial max sum
   if (arr.length < num) return null;
   for (let i = 0; i < num; i++) {
     maxSum += arr[i];
   }
   tempSum = maxSum;
+  // loop through sliding window style
   for (let i = num; i < arr.length; i++) {
+    // take original sum
+    // subtract first element in the test case 2 at 0
+    // add next element in test case 2 at 3
     tempSum = tempSum - arr[i - num] + arr[i];
+    // only replace max if tempSum is larger
     maxSum = Math.max(maxSum, tempSum);
   }
+  // return ultimate max
   return maxSum;
 }
 
