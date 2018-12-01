5448a783fbcc4154295fcb2e172b04e40b60c3aa
Travis Cooper <dozertdc@gmail.com>
2018-11-30 21:19:36 -0500

    Update sameFrequency.js

--- Basic/Frequency_Counter/sameFrequency.js
+++ Basic/Frequency_Counter/sameFrequency.js
@@ -1,11 +1,16 @@
 function sameFrequency(num1, num2){
+  // convert to strings
   num1 = num1.toString();
   num2 = num2.toString();
+  // if not the same length then cannot have same frequency
   if(num1.length !== num2.length) return false;
+  // create hashmap of counts of first number
   num1Counts = {};
   for(let num of num1){
       num1Counts[num] = num1Counts[num] ? num1Counts[num]+1 : 1;
   }
+  // decrement value associated with key when the number
+  // comes up in number 2
   for(let num of num2){
       if(!num1Counts[num]){
           return false;
