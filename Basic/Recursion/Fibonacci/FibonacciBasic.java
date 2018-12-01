48ee7758f292a5386344c0f0412cdd99c82676ab
Travis Cooper <dozertdc@gmail.com>
2018-12-01 16:08:36 -0500

    Update FibonacciBasic.java

--- Basic/Recursion/Fibonacci/FibonacciBasic.java
+++ Basic/Recursion/Fibonacci/FibonacciBasic.java
@@ -1,11 +1,13 @@
 public class FibonacciBasic {
-
+    
     public int fib(int n) {
+        // terminating conditions
         if (n <= 0) {
             return 0;
         } else if (n == 1) {
             return 1;
         } else {
+            // recursive calls
             return fib(n - 1) + fib(n - 2);
         }
     }
